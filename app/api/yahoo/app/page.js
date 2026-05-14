ac91f622dc80f2de620e33c7f0abd4e228b0df67
vikaskhandekar30 <vikas_khandekar@hotmail.com>
2026-05-14 18:46:53 +0100

    Create page.js

---
+++ app/api/yahoo/app/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Scanner Summary Viewer</h1>
+      <p>Your Yahoo Finance API route is working.</p>
+    </div>
+  );
+}
