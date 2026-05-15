a90740c18667f4f2d1f433afd1d0184532b07744
vikaskhandekar30 <vikas_khandekar@hotmail.com>
2026-05-15 09:59:40 +0100

    Delete app/api/yahoo/app directory

--- app/api/yahoo/app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Scanner Summary Viewer</h1>
-      <p>Your Yahoo Finance API route is working.</p>
-    </div>
-  );
-}
