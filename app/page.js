226f3c7452138a7196b50b09f3e6291b75a99585
vikaskhandekar30 <vikas_khandekar@hotmail.com>
2026-05-15 10:09:34 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Scanner Summary Viewer</h1>
+      <p>Your API is live and working.</p>
+    </div>
+  );
+}
